94106326d23614e3362414ec01aae024993d8736
allenki1eo <allenkileo7@gmail.com>
2026-04-29 18:23:09 +0100

    Update history page

--- src/app/our-history/page.tsx
+++ src/app/our-history/page.tsx
@@ -12,7 +12,7 @@ const eras = [
     title: "The Founding",
     description:
       "Born from a vision to craft premium beverages that reflect the spirit of East Africa, East African Spirits was established in 2017. Starting with a small team and bold ambitions, we set out to redefine quality in the regional beverage market.",
-    images: ["/images/east4.png", "/images/east1.png", "/images/east2.png", "/images/east3.png"],
+    images: ["/images/east7.png", "/images/east5.png", "/images/.png", "/images/east6.png"],
   },
   {
     id: "expansion",
